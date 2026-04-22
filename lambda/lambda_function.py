216e229327694c0d54c5f3ea52f8dd489f78dbfb
NithinRallapalli <rallapallinithin@gmail.com>
2026-04-22 19:18:45 +0530

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,1 +1,28 @@
+import json
+import boto3
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('UsersData')
+
+def lambda_handler(event, context):
+    try:
+        # Get all users from DynamoDB
+        response = table.scan()
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps(response['Items'])
+        }
+
+    except Exception as e:
+        print("Error:", str(e))
+
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'error': str(e)
+            })
+        }
